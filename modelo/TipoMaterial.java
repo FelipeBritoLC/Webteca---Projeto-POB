6eae92de5b5e7f52fd7c15364529ef9a5b486401
Felipe de Brito <felipebl1512@gmail.com>
2025-05-15 22:30:32 -0300

    Classes modelo

---
+++ modelo/TipoMaterial.java
@@ -0,0 +1,20 @@
+public class TipoMaterial {
+    private String nome; //video, site, apostila, livro, slides, etc
+
+    public TipoMaterial (String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString() {
+        return "Tipo Material [nome=" + nome + "]";
+    }
+      
+}
